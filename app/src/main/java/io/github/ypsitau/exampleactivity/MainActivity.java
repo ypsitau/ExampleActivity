c391eec7811e42c872561f090f895a66a9082958
ypsitau <ypsitau@nifty.com>
2018-04-20 12:47:25 +0900

    implementing

--- app/src/main/java/io/github/ypsitau/exampleactivity/MainActivity.java
+++ app/src/main/java/io/github/ypsitau/exampleactivity/MainActivity.java
@@ -3,36 +3,57 @@ package io.github.ypsitau.exampleactivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-    final static String KEY_FRUIT = "fruit";
-    final static String KEY_COLOR = "color";
-
+    private enum Key {
+        CntClickFruit,
+        CntClickColor,
+    }
+    int cntClickFruit;
+    int cntClickColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(getString(R.string.app_name), "onCreate()");
         setContentView(R.layout.activity_main);
         RadioGroup radioGroup_fruit = findViewById(R.id.radioGroup_fruit);
         RadioGroup radioGroup_color = findViewById(R.id.radioGroup_color);
-/*
-        radioGroup_fruit.check(R.id.radioButton_banana);
-        radioGroup_color.check(R.id.radioButton_blue);
-*/
+        //radioGroup_fruit.check(R.id.radioButton_banana);
+        //radioGroup_color.check(R.id.radioButton_blue);
+        radioGroup_fruit.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Check if the change is caused by user input, not the device rotation.
+                if (!group.findViewById(checkedId).isPressed()) return;
+                Log.i(getString(R.string.app_name), String.format("onCheckedChanged(): fruit=%s",
+                        ((RadioButton)group.findViewById(checkedId)).getText()));
+                cntClickFruit++;
+            }
+        });
+        radioGroup_color.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Check if the change is caused by user input, not the device rotation.
+                if (!group.findViewById(checkedId).isPressed()) return;
+                Log.i(getString(R.string.app_name), String.format("onCheckedChanged(): color=%s",
+                        ((RadioButton)group.findViewById(checkedId)).getText()));
+                cntClickColor++;
+            }
+        });
         if (savedInstanceState == null) {
             Log.i(getString(R.string.app_name), "savedInstanceState == null");
-            radioGroup_fruit.check(R.id.radioButton_banana);
-            radioGroup_color.check(R.id.radioButton_blue);
+            cntClickFruit = 0;
+            cntClickColor = 0;
         } else {
+            cntClickFruit = savedInstanceState.getInt(Key.CntClickFruit.name());
+            cntClickColor = savedInstanceState.getInt(Key.CntClickColor.name());
             Log.i(getString(R.string.app_name), String.format("savedInstanceState != null; fruit:%d color:%d",
-                    savedInstanceState.getInt(KEY_FRUIT), savedInstanceState.getInt(KEY_COLOR)));
-            radioGroup_fruit.check(savedInstanceState.getInt(KEY_FRUIT));
-            radioGroup_color.check(savedInstanceState.getInt(KEY_COLOR));
+                    cntClickFruit, cntClickColor));
         }
         super.onCreate(savedInstanceState);
     }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -68,18 +89,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.i(getString(R.string.app_name), "onSaveInstanceState()");
-
+        outState.putInt(Key.CntClickFruit.name(), cntClickFruit);
+        outState.putInt(Key.CntClickColor.name(), cntClickColor);
     }
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        RadioGroup radioGroup_fruit = findViewById(R.id.radioGroup_fruit);
-        RadioGroup radioGroup_color = findViewById(R.id.radioGroup_color);
-        Log.i(getString(R.string.app_name), String.format("onRestoreInstanceState(); fruit:%d color:%d",
-                radioGroup_fruit.getCheckedRadioButtonId(), radioGroup_color.getCheckedRadioButtonId()));
-        savedInstanceState.putInt(KEY_FRUIT, radioGroup_fruit.getCheckedRadioButtonId());
-        savedInstanceState.putInt(KEY_COLOR, radioGroup_color.getCheckedRadioButtonId());
-        Log.i(getString(R.string.app_name), String.format("immediately fruit:%d color:%d",
-                savedInstanceState.getInt(KEY_FRUIT), savedInstanceState.getInt(KEY_COLOR)));
     }
 }
